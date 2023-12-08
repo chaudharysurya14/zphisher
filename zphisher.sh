770ff48af37080bdece86d88dfa7590877350693
SURYADEV CHAUDHARY <114549294+chaudharysurya14@users.noreply.github.com>
2023-12-08 12:50:17 +0530

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -210,7 +210,7 @@ banner() {
 		${ORANGE}  / / | '_ \| '_ \| / __| '_ \ / _ \ '__|
 		${ORANGE} / /__| |_) | | | | \__ \ | | |  __/ |   
 		${ORANGE}/_____| .__/|_| |_|_|___/_| |_|\___|_|   
-		${ORANGE}      | |                                
+		${ORANGE}      | |          By :- Cyber Surya                      
 		${ORANGE}      |_|                ${RED}Version : ${__version__}
 
 		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
